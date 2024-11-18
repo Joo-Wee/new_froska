01c816c06d5e9cc2a372570c6354af4e00787878
Joo-Wee <maphadathrowmhijoo@gmail.com>
2024-11-18 22:18:56 +0000

    Adding

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'header-color': '#2E1A47',
+        'header-color_btn': '#6e57e0',
+        'deep-purple': '#2E1A47',
+        'turqoise': '#40E0D0',
+        'body-color': '#9e96eb'
+    },
+
+      backgroundImage: {
+        'hero-pattern': "url('./media/Landing page.png')",
+        'games-pattern': "url('./media/OBSKLD0.jpg')"
+      }
+  },
+  plugins: [],
+}
+}
